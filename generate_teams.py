2507e9c90cd858211f72125227113769bd12006a
nchlab <bt17684@qmul.ac.uk>
2020-04-11 02:18:16 +0100

    update team number added

--- generate_teams.py
+++ generate_teams.py
@@ -16,8 +16,8 @@ root = tk.Tk()
 # root.iconbitmap("./icons/icon.ico")
 
 team_data = {}
-label_object = list()
-player_checkbox = list()
+# label_object = list()
+# player_checkbox = list()
 
 class App(tk.Frame):
     def __init__(self, master):
@@ -31,12 +31,18 @@ class App(tk.Frame):
         self.label_object = list()
         self.player_checkbox = list()
         self.shuffled_teams = list()
+        self.username = None
+        self.menu_opt = None
+
+        self.settings_window = None
+        self.updated_text = None
 
         self.frame1 = tk.LabelFrame(self.master, padx=5, pady=5)
         self.frame1.grid(row=5, column =0)
 
-        self.frame2 = tk.LabelFrame(root, padx=5, pady=5) 
-        self.frame2.grid(row=5, column =1)
+        self.frame = None
+        # self.frame2 = tk.LabelFrame(root, padx=5, pady=5) 
+        # self.frame2.grid(row=5, column =1)
 
         self.initialise_data()
 
@@ -54,23 +60,32 @@ class App(tk.Frame):
     def initialise_data(self):
 
         self.load_data()
-        self.generate_labels(self.num_of_team)
+        self.generate_labels()
         self.generate_player()
         self.generate_button()
 
     def generate_player(self):
 
+        self.frame2 = tk.LabelFrame(root, padx=5, pady=5) 
+        self.frame2.grid(row=5, column =1)
+
         column = 6
         row = 5
         self.num_of_players = len(self.team_list)
+        self.player_checkbox = []
 
         for i in range(self.num_of_players):
             self.player_checkbox.append(Person(column, row, self.team_list[i],self.frame2))
             row+=1
 
-    def generate_labels(self, n):
+        
 
-        for _ in range(n):
+    def generate_labels(self):
+
+        
+        self.label_object = []
+
+        for _ in range(self.num_of_team):
             self.label_object.append(tk.Label(self.frame1, text="", font=("Arial Bold", 10)))
 
     def display_list(self):
@@ -92,6 +107,84 @@ class App(tk.Frame):
 
         btn2 = tk.Button(self.master, text="GENERATE TEAMS", bg="#bbede8", fg="#0003c9",padx= 20, pady=14, command=lambda: self.display_list())
         btn2.grid(column=0, row=4)
+
+        settings_btn = tk.Button(self.master, text="Settings", bg="#e0dcdd", fg="#ff195e",padx= 20, pady=14, command=lambda: self.change_settings())
+        settings_btn.grid(column=1, row=4)
+
+    
+    def change_settings(self):
+        self.settings_window = tk.Toplevel()
+        self.settings_window.title("Settings - Modify Team List")
+        self.settings_window.geometry('400x300')
+
+        self.menu_opt = tk.IntVar()
+        self.menu_opt.set(self.num_of_team)
+
+        team_options = [2,3,4,5,6,7,8,9,10]
+
+        dropdown_label = tk.Label(self.settings_window, text="Number of Teams")
+        dropdown_label.grid(row=0, column=0)
+
+        team_Dropdown = tk.OptionMenu(self.settings_window, self.menu_opt, *team_options)
+        team_Dropdown.grid(row=0, column=1)
+
+        update_btn = tk.Button(self.settings_window, text="Update", bg="#e0dcdd", fg="#ff195e",command=lambda: self.update_team_list("update"))
+        update_btn.grid(row=1, column=0, columnspan=2, pady=10, padx=10, ipadx=60)
+
+        username_label = tk.Label(self.settings_window, text="Username")
+        username_label.grid(row=3, column=0)
+
+        self.username = tk.Entry(self.settings_window, width=30)
+        self.username.grid(row=3, column =1)
+
+        add_btn = tk.Button(self.settings_window, text="Add User", bg="#e0dcdd", fg="#ff195e",command=lambda: self.update_team_list("add"))
+        add_btn.grid(row=4, column=0, columnspan=2, pady=10, padx=10, ipadx=60)
+
+
+    def update_team_list(self, mode="update"):
+
+        json_file = open("team_list.json", "r")
+        data = json.load(json_file) 
+        json_file.close()
+
+        if mode == "add":
+            new_data = self.username.get()
+            self.username.delete(0, 'end')
+            new_data = new_data.strip()
+            if new_data:
+                data["names"].append(new_data)
+            else:
+                return
+        elif mode == "delete":
+            pass
+        else:
+            self.num_of_team = self.menu_opt.get()
+            data["numOfTeam"] = self.num_of_team
+            self.updated_text = tk.Label(self.settings_window, text='Number Updated!', fg='green')
+            self.updated_text.grid(row=1, column=3)
+            self.updated_text.after(1000, self.updated_text.destroy)
+            
+            # self.updated_text.after(1000, lambda: self.updated_text.configure(text=""))
+            
+
+
+        json_file = open("team_list.json", "w+")
+        json_file.write(json.dumps(data, indent=4))
+        json_file.close()
+
+        if mode == "add":
+            self.player_checkbox.append(Person(14, 5, new_data,self.frame2))
+            # for i in self.player_checkbox:
+            #     i.frame2.destroy()
+            #     i.chk.destroy()
+            #     i.destroy()
+            # self.generate_player()
+
+        for i in self.label_object:
+            i.destroy()
+
+        self.generate_labels()
+
 
 
 
@@ -133,75 +226,6 @@ def shuffle_teams(data, n):
 
     return teams
 
-
-
-
-
-
-
-def change_settings(num_of_team):
-    settings_window = tk.Toplevel()
-    settings_window.title("Settings - Modify Team List")
-    settings_window.geometry('400x300')
-
-    menu_opt = tk.IntVar()
-    menu_opt.set(num_of_team)
-
-    team_options = [2,3,4,5,6,7,8,9,10]
-
-    dropdown_label = tk.Label(settings_window, text="Last Name")
-    dropdown_label.grid(row=0, column=0)
-
-    team_Dropdown = tk.OptionMenu(settings_window, menu_opt, *team_options)
-    team_Dropdown.grid(row=0, column=1)
-
-    update_btn = tk.Button(settings_window, text="Update", bg="#e0dcdd", fg="#ff195e",command=lambda: update_team_list(menu_opt.get(), "update"))
-    update_btn.grid(row=1, column=0, columnspan=2, pady=10, padx=10, ipadx=60)
-
-
-    username_label = tk.Label(settings_window, text="Last Name")
-    username_label.grid(row=2, column=0)
-
-    username = tk.Entry(settings_window, width=30)
-    username.grid(row=2, column =1)
-
-    add_btn = tk.Button(settings_window, text="Add User", bg="#e0dcdd", fg="#ff195e",command=lambda: update_team_list(username.get(), "add"))
-    add_btn.grid(row=3, column=0, columnspan=2, pady=10, padx=10, ipadx=60)
-
-
-    def update_team_list(new_data, mode="update"):
-
-        json_file = open("team_list.json", "r")
-        data = json.load(json_file) 
-        json_file.close()
-
-        if mode == "add":
-            username.delete(0, 'end')
-            new_data = new_data.strip()
-            if new_data:
-                data["names"].append(new_data)
-            else:
-                return
-        elif mode == "delete":
-            pass
-        else:
-            data["numOfTeam"] = new_data
-
-
-        json_file = open("team_list.json", "w+")
-        json_file.write(json.dumps(data, indent=4))
-        json_file.close()
-
-
-
-
-# def generate_button(num_of_team): 
-
-#     btn2 = tk.Button(root, text="GENERATE TEAMS", bg="#bbede8", fg="#0003c9",padx= 20, pady=14, command=lambda: display_list(num_of_team))
-#     btn2.grid(column=0, row=4)
-
-    # settings_btn = tk.Button(root, text="Settings", bg="#e0dcdd", fg="#ff195e",padx= 20, pady=14, command=lambda: change_settings(num_of_team))
-    # settings_btn.grid(column=1, row=4)
 
 
 
